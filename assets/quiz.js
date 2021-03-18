03af95aaacffeba1d455256ce70dee6b566e6956
Michelle <michey.asmah21@gmail.com>
2021-03-18 12:47:01 -0400

    show first question

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,11 +1,39 @@
-var startQuiz = document.querySelector("#start");
-var homeScreen = document.querySelector("#home");
+var startQuiz = document.querySelector("#start")
+var homeScreen = document.querySelector("#home")
 var questionScreen = document.querySelector(".questions")
+var shownQuestions = document.querySelector("#question")
+var ansOptions = document.querySelector("#choices")
+var displayedQuestions, questionIndex
+var questions = [
+    {
+      question: "<h1>What is the largest Heading Tag in HTML?</h1>",
+      answers: [
+          {text: "<button>H12</button>", correct: false},
+          {text: "H6", correct: false},
+          {text: "H1", correct: true},
+          {text: "H8", correct: false}
+      ]
+    }
+]
 
 var startGame = function () {
     homeScreen.setAttribute("class", "hide")
+    displayedQuestions = questions
+    questionIndex = 0
     questionScreen.removeAttribute("class", "hide")
+    nextQuestion()
 }
+
+
+var nextQuestion = function () {
+    showQuestions(displayedQuestions[questionIndex]) 
+}
+
+var showQuestions = function (question) {
+    shownQuestions.innerHTML = question.question
+}
+
+
 
 
 startQuiz.addEventListener("click", startGame);
