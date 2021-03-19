f769280e3e31e83df0f7e1212091df3316159058
Michelle <michey.asmah21@gmail.com>
2021-03-18 20:53:29 -0400

    questions array complete

--- assets/quiz.js
+++ assets/quiz.js
@@ -13,7 +13,97 @@ var questions = [
           { text: "H1", correct: true },
           { text: "H8", correct: false }
       ]
-    }
+    },
+    
+    {
+      question: "<h1>What symbol is added to most HTML closing tags?</h1>",
+      answers: [
+          {text: "/", correct: true},
+          {text: "*", correct: false},
+          {text: ")", correct: false},
+          {text: "]", correct: false}
+      ]  
+    },
+
+    {
+      question: "<h1>/eggs/bacon/tomatoes.png is an example of...?</h1>",
+      answers: [
+         {text: "A relative path", correct: true},
+         {text: "An absolute path", correct: false},
+         {text: "A URL", correct: false},
+         {text: "A sandwich path", correct: false}
+      ]  
+    },
+
+    {
+        question: "<h1>A boolean data type is?</h2>",
+        answers: [
+            { text: "True", correct: false },
+            { text: "False", correct: false },
+            { text: "True or False", correct: true },
+            { text: "None of the above", correct: false }
+        ]  
+      },
+
+      {
+        question: "<h1>Cd in the command line stands for what?</h1>",
+        answers: [
+            { text: "Change directory", correct: true },
+            { text: "Compact disk", correct: false },
+            { text: "Command discharge", correct: false },
+            { text: "Computer database", correct: false }
+        ]  
+      },
+
+      {
+        question: "<h1>What does git checkout do?</h1>",
+        answers: [
+            { text: "Switches the git branch", correct: true },
+            { text: "Checks git status", correct: false },
+            { text: "Merges git branches", correct: false },
+            { text: "Updates your repository", correct: false }
+        ]  
+      },
+
+      {
+        question: "<h1>Wireframing in web development means ...</h1>",
+        answers: [
+            { text: "Connecting git branches", correct: false },
+            { text: "Connecting wires to make sure your computer is working", correct: false },
+            { text: "A layout of content and functionality of a webpage", correct: true },
+            { text: "Merging git branches", correct: false }
+        ]  
+      },
+
+      {
+        question: "<h1>The CSS box model is compromised of</h1>",
+        answers: [
+            { text: "Margin, content, padding, border", correct: true },
+            { text: "Border, flex box, content, padding", correct: false },
+            { text: "Margin, float, content, flex box", correct: false },
+            { text: "Float, flex box, content, border", correct: false }
+        ]  
+      },
+
+      {
+        question: "<h1>for (i = 0; i < 7; i++) {Console.log(“I love coding”)} is an example of</h1>",
+        answers: [
+            { text: "A loop", correct: false },
+            { text: "An iteration", correct: false },
+            { text: "A for statement", correct: false },
+            { text: "All of the above", correct: true }
+        ]  
+      },
+
+      {
+        question: "<h1>Which of the below is not an example of DOM manipulation?</h1>",
+        answers: [
+            { text: "document.body", correct: false },
+            { text: "document.getElementById('myBtn').addEventListener('click', displayMessage)", correct: false },
+            { text: "listEl.appendChild(li1)", correct: true },
+            { text: "None of the above", correct: false }
+        ]  
+      },
 ]
 
 var startGame = function () {
@@ -25,6 +115,7 @@ var startGame = function () {
 }
 
 var nextQuestion = function () {
+    reset()
     showQuestions(displayedQuestions[questionIndex]) 
 }
 
@@ -35,17 +126,26 @@ var showQuestions = function (question) {
         button.innerText = answer.text
         button.setAttribute("class", "btn")
         if (answer.correct) {
-            button.dataset.correct = choice.correct
+            button.dataset.correct = answer.correct
         }
         button.addEventListener("click", answerChoice)
         ansOptions.appendChild(button)
     })
 }
     
+var reset = function () {
+    while (ansOptions.firstChild) {
+        ansOptions.removeChild(ansOptions.firstChild)
+    }
+}
 
 var answerChoice = function (event) {
+    var selectedButton = event.target
+    var correct = selectedButton.dataset.correct
 
 }
+
+
 
 
 
