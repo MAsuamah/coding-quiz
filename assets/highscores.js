e0da8a13cd2e21828eed4c5c91dfd6bae9985670
Michelle <michey.asmah21@gmail.com>
2021-03-21 00:53:20 -0400

    comments for highscores js

--- assets/highscores.js
+++ assets/highscores.js
@@ -1,16 +1,21 @@
+//Reference for code https://www.youtube.com/watch?v=DFhmNLKwwGw&ab_channel=JamesQQuick
+
 var username = document.querySelector("#username")
 var saveScoreBtn = document.querySelector("#saveScoreBtn");
 var highScore = document.querySelector("#score");
 var mostRecentScore = localStorage.getItem("mostRecentScore");
+
+//If there are no available highscores in local storage return an empty array
 var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
+//Highscore page will only show a max of 5 scores
 var maxHighScores = 5
 
 
 highScore.innerText = "Your final score is " + mostRecentScore;
 
+//When the save button is clicked score and name will be saved as key-value pairs in the highScores array
 saveHighScore = e => {
-  console.log("clicked!")
   e.preventDefault()
 
   var score = {
