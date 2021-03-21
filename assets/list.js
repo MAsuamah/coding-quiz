c52dbe5ee6b1da007efac9b97ab35337bf654aab
Michelle <michey.asmah21@gmail.com>
2021-03-21 00:37:34 -0400

    comments for list js

--- assets/list.js
+++ assets/list.js
@@ -1,8 +1,9 @@
+//Reference for code https://www.youtube.com/watch?v=o3MF_JmQxYg&t=188s&ab_channel=JamesQQuick
+
 var highScoresList = document.querySelector("#highScoresList")
 var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
-console.log(highScores)
-
+//Retrieve scores array from local storage and return each key and value as a list element to create the highscores page
 highScoresList.innerHTML = (highScores.map(score => {
   return (`<li class="high-score">${score.name} - ${score.score}</li>`)
 }).join("")
